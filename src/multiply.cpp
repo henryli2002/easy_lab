9e094b45cda74b4883b19cab72f0b38f9624fb1f
HenryLi <3135601878@qq.com>
2023-10-27 15:29:23 +0000

    test

--- src/multiply.cpp
+++ src/multiply.cpp
@@ -5,5 +5,8 @@
 
 void matrix_multiplication(double matrix1[N][M], double matrix2[M][P], double result_matrix[N][P])
 {
-
+    for(int row = 0; row < N; ++row)
+            for(int col = 0; col < P; ++col)
+                for(int mid = 0; mid < M; ++mid)
+                    result_matrix[row][col] += matrix1[row][mid] * matrix2[mid][col];
 }
